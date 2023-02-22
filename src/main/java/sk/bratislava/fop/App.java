86f16fda4c7e75fd29c5e160499f3d9165550a72
Kristian Duda <kristianduda83@gmail.com>
2023-02-22 19:26:38 +0100

    get port from env

--- src/main/java/sk/bratislava/fop/App.java
+++ src/main/java/sk/bratislava/fop/App.java
@@ -3,9 +3,24 @@ package sk.bratislava.fop;
 import io.javalin.Javalin;
 
 public class App {
+    public static int getPort() {
+        int port = 7000;
+        String envPort = System.getenv("PORT");
+        if (envPort != null) {
+            try {
+                port = Integer.parseInt(envPort);
+            } finally {
+
+            }
+        }
+        return port;
+    }
+
     public static void main(String[] args) {
+        int port = getPort();
+
         Javalin app = Javalin.create(/* config */)
                 .get("/", ctx -> ctx.result("Hello World"))
-                .start(7000);
+                .start(port);
     }
 }
