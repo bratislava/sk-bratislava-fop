6890b5dd74291082a5213e653619b015e1c5ed16
Martin Pinter <martin.pinter@bratislava.sk>
2023-11-13 17:30:43 +0100

    Donwgrade Apache FOP to 1.1

--- src/main/java/sk/bratislava/fop/FopService.java
+++ src/main/java/sk/bratislava/fop/FopService.java
@@ -2,7 +2,6 @@ package sk.bratislava.fop;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.InputStream;
 import java.io.StringReader;
 
 import javax.xml.transform.Result;
@@ -22,10 +21,16 @@ public class FopService {
     }
 
     public byte[] transform(String data, String xslt) throws Exception {
-        InputStream config = getClass().getClassLoader().getResourceAsStream("fop.xconf");
+        // fop must use version 1.1 according to current legislation
+        // in case it ever gets updated, the following lines work for configuration of 2.x
+        // + we can get rid of the avalon dependencies in lib directory, which are needed only for 1.1
+        
+        // InputStream config = getClass().getClassLoader().getResourceAsStream("fop.xconf");
+        // final FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI(), config);
 
         // configure fopFactory as desired
-        final FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI(), config);
+        final FopFactory fopFactory = FopFactory.newInstance();
+        fopFactory.setUserConfig(new File("./resources/fop.xconf"));
         FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
         // configure foUserAgent as desired
 
@@ -40,7 +45,7 @@ public class FopService {
             Transformer transformer = factory.newTransformer(toStreamSource(xslt));
 
             // Set the value of a <param> in the stylesheet
-            transformer.setParameter("versionParam", "2.0");
+            transformer.setParameter("versionParam", "1.1");
 
             // Resulting SAX events (the generated FO) must be piped through to FOP
             Result res = new SAXResult(fop.getDefaultHandler());
